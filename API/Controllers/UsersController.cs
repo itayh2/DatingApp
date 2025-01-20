38fdb7184aebf5bdbed6a80e7f657bc86a752cbc
Itay Hasid <itay@middiamonds.com>
2025-01-20 09:31:04 +0200

    change in gitignore file and usersController added async

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,6 +1,7 @@
 using API.Data;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 [ApiController]
@@ -8,10 +9,21 @@ namespace API.Controllers;
 public class UsersController(DataContext context) : ControllerBase
 {
     [HttpGet]
-    public ActionResult<IEnumerable<AppUser>> GetUsers()
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
     {
-        var users = context.Users.ToList();
-        
+        var users = await context.Users.ToListAsync();
+
         return users;
     }
+
+    [HttpGet("{id:int}")] // /api/users/1
+    public async Task<ActionResult<AppUser>> GetUser(int id)
+    {
+        var user = await context.Users.FindAsync(id);
+
+        if (user == null) return NotFound();
+
+        return user;
+    }
+
 }
