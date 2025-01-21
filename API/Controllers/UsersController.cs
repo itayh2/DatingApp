84afdd1c24381f5ed8e389a19752322553d4bae5
Itay Hasid <itay@middiamonds.com>
2025-01-21 10:52:03 +0200

    adding accountController and RegisterDto

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -4,9 +4,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
-[ApiController]
-[Route("api/[controller]")] // /api/users
-public class UsersController(DataContext context) : ControllerBase
+
+public class UsersController(DataContext context) : BaseApiController
 {
     [HttpGet]
     public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
