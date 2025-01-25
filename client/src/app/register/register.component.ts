65fc3fdd1dfc6da09a0778702d57823b428dbe27
Itay Hasid <itay@middiamonds.com>
2025-01-25 22:17:43 +0200

    add register method

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
-import { Component, input, output } from '@angular/core';
+import { Component, inject, input, output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { AccountService } from '../_services/account.service';
 
 @Component({
   selector: 'app-register',
@@ -9,14 +10,19 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './register.component.css',
 })
 export class RegisterComponent {
-  usersFromHomeComponent = input.required<any>(); // new version
+  private accountService = inject(AccountService);
   cancelRegister = output<boolean>(); // new version
-  // @Input() usersFromHomeComponent: any; old version
   //@Output() cancelRegister = new EventEmitter(); old version
   model: any = {};
 
   register() {
-    console.log(this.model);
+    this.accountService.register(this.model).subscribe({
+      next: (response) => {
+        console.log(response);
+        this.cancel();
+      },
+      error: (error) => console.log(error),
+    });
   }
 
   cancel() {
