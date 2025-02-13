73e93ffd7a32c0481268d8ca5b9d6e8cb1a19b9f
Itay Hasid <itay@middiamonds.com>
2025-02-13 12:10:23 +0200

    add calculateAge to AutoMapperProfiles

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -11,26 +11,24 @@ using Microsoft.Extensions.DependencyModel.Resolution;
 namespace API.Controllers;
 
 [Authorize]
-public class UsersController(IUserRepository userRepository, IMapper mapper) : BaseApiController
+public class UsersController(IUserRepository userRepository) : BaseApiController
 {
     [HttpGet]
     public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
     {
-        var users = await userRepository.GetUsersAsync();
+        var users = await userRepository.GeteMembersAsync();
 
-        var usersToReturn = mapper.Map<IEnumerable<MemberDto>>(users);
-
-        return Ok(usersToReturn);
+        return Ok(users);
     }
 
     [HttpGet("{username}")] // /api/users/lisa
     public async Task<ActionResult<MemberDto>> GetUser(string username)
     {
-        var user = await userRepository.GetUserByUsernameAsync(username);
+        var user = await userRepository.GetMemberAsync(username);
 
         if (user == null) return NotFound();
 
-        return mapper.Map<MemberDto>(user);    
+        return user;    
     }
 
 }
