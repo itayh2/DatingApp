aebf283cfdbad5a1793640445dad63d70d02949d
Itay Hasid <itay@middiamonds.com>
2025-03-04 12:33:19 +0200

    adding details page with tabset

--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -2,11 +2,12 @@ import { Component, inject, input, OnInit } from '@angular/core';
 import { MembersService } from '../../_services/members.service';
 import { ActivatedRoute } from '@angular/router';
 import { Member } from '../../_models/member';
+import { TabsModule } from 'ngx-bootstrap/tabs';
 
 @Component({
   selector: 'app-member-detail',
   standalone: true,
-  imports: [],
+  imports: [TabsModule],
   templateUrl: './member-detail.component.html',
   styleUrl: './member-detail.component.css',
 })
@@ -14,7 +15,7 @@ export class MemberDetailComponent implements OnInit {
   private memberService = inject(MembersService);
   private route = inject(ActivatedRoute);
   member?: Member;
-  
+
   ngOnInit(): void {
     this.loadMember();
   }
