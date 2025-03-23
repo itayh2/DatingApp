56970530f7f181ef464d7c496cc410b9c94dad01
Itay Hasid <itay@middiamonds.com>
2025-03-23 16:12:10 +0200

    design inbox - still have one problem with Outbox

--- client/src/app/messages/messages.component.ts
+++ client/src/app/messages/messages.component.ts
@@ -1,10 +1,22 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { MessageService } from '../_services/message.service';
+import { ButtonsModule } from 'ngx-bootstrap/buttons';
+import { FormsModule } from '@angular/forms';
+import { TimeagoModule } from 'ngx-timeago';
+import { Message } from '../_models/message';
+import { RouterLink } from '@angular/router';
+import { PaginationModule } from 'ngx-bootstrap/pagination';
 
 @Component({
   selector: 'app-messages',
   standalone: true,
-  imports: [],
+  imports: [
+    ButtonsModule,
+    FormsModule,
+    TimeagoModule,
+    RouterLink,
+    PaginationModule,
+  ],
   templateUrl: './messages.component.html',
   styleUrl: './messages.component.css',
 })
@@ -26,6 +38,11 @@ export class MessagesComponent implements OnInit {
     );
   }
 
+  getRoute(message: Message) {
+    if (this.container === 'Outbox')
+      return `/members/${message.recipientUsername}`;
+    else return `/members/${message.senderUsername}`;
+  }
   pageChanged(event: any) {
     if (this.pageNumber !== event.page) {
       this.pageNumber = event.page;
